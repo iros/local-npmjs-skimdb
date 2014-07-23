b4d996c70669bc6c21e7c06dc6744272727a0b00
Irene Ros <imirene@gmail.com>
2014-07-23 15:55:36 -0400

    added get all documents script

---
+++ scripts/get_all_docs.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+
+var db = require('nano')('http://192.168.33.31:5984/registry'),
+
+  // if the import breaks, set the startkey: '' to the last imported lib.
+  params   = {include_docs: true, limit: 200, skip: 0, startkey:'aone'},
+
+  has_docs = true;
+
+
+function getDocs(params) {
+  db.list(params, function(error, body, headers) {
+
+    if (error) { console.log(error); return; }
+
+    console.log("fetching", params.skip, "-", params.skip + params.limit);
+
+    if (body.rows.length === 0) {
+      has_docs = false;
+    } else {
+
+      var keys = [];
+      body.rows.forEach(function(doc) {
+        keys.push(doc.id);
+        if (doc.id !== "_design/app" && doc.id !== "_design/scratch") {
+          var filepath = "data/packages/" + doc.id + ".json";
+          fs.writeFileSync(filepath, JSON.stringify(doc));
+        }
+      });
+
+      console.log("wrote", keys.join(" ,"));
+      params.skip = params.skip + params.limit;
+
+      getDocs(params);
+    }
+  });
+}
+
+getDocs(params);
