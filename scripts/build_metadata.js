eac40640f351196d5ab3382bb5cd56d09c4f0e08
Irene Ros <imirene@gmail.com>
2014-09-23 16:52:33 -0400

    updating metadata build script to add row transformation

--- scripts/build_metadata.js
+++ scripts/build_metadata.js
@@ -9,19 +9,28 @@ function addRow(package, doc, data) {
 
   if (exclude.indexOf(package) === -1) {
 
+    var latest_version;
+    var maintainers;
+    var num_versions;
+    var versem;
+    var keywords;
+
     try {
 
       // find latest version
-      var latest_version = packageUtils.findLatestVersion(doc);
+      latest_version = packageUtils.findLatestVersion(doc);
 
       // find maintainers
-      var maintainers = packageUtils.findMaintainers(doc);
+      maintainers = packageUtils.findMaintainers(doc);
 
       // find version numbers
-      var num_versions = packageUtils.findNumberOfVersions(doc);
+      num_versions = packageUtils.findNumberOfVersions(doc);
 
       // get semver breakdown.
-      var versem = packageUtils.getSemver(latest_version);
+      versem = packageUtils.getSemver(latest_version);
+
+      // get keywords
+      keywords = packageUtils.getKeywords(doc, latest_version);
 
       // build row
       var row = [
@@ -33,7 +42,8 @@ function addRow(package, doc, data) {
         doc.time ? doc.time.created : doc.ctime,
         doc.time ? doc.time.modified : doc.mtime,
         maintainers.length,
-        num_versions
+        num_versions,
+        keywords
       ];
 
       i++;
@@ -44,13 +54,20 @@ function addRow(package, doc, data) {
 
       return row;
     } catch(e) {
-      console.log(package, e);
-      exclude.push(package);
+      console.log(package, e, latest_version);
+      exclude.push([package, latest_version]);
     }
   }
 }
 
 forAllPackages(addRow, "packages_meta.csv", [
   ["id", "latest_version", "version_major", "version_minor", "version_patch",
-  "created", "modified", "maintainer_count", "version_count"]
-]);
+  "created", "modified", "maintainer_count", "version_count", "keywords"]
+], function(rows) {
+  var transformed_rows = [];
+  rows.forEach(function(r) {
+    r[9] = r[9].toString();
+    transformed_rows.push(r);
+  });
+  return transformed_rows;
+});
